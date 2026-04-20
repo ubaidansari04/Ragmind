44e5368f7396950bf5197a5742906d0d63dfa45b
ubaidansari04 <ubaidansari04052004@gmail.com>
2026-04-20 18:46:00 +0530

    update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import store from "./store";
+import store from "./store/Index";
 import App from "./App";
 import "./index.css";
 
